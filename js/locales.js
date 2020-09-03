c36ad684f22b2b10e1fc9d071e38d77bbd6d2b7c
Juan Esteban Zapata <juanes0202@gmail.com>
2020-09-02 19:32:45 -0500

    Listando datos de los locales en el index

---
+++ js/locales.js
@@ -0,0 +1,159 @@
+var locales = [{
+        "nombre": "Jhon perrada",
+        "facebook": "https://www.facebook.com/JhonPerrada",
+        "instagram": "http://instagram.com/jhonperrada/",
+        "descripcion": "Estadero de cocina callejera y de autor. 14 años de historia en el Corregimiento de San Cristóbal. Aquí Vale la pena esperar y compartir en Familia. Bienvenidos. ",
+        "horario": "Horario de atención: lunes, miércoles y viernes: cerrado  Jueves, viernes, sábado y domingo: de 17:00 a 22:00 ",
+        "telefono": "302 2296614",
+        "direccion": "Calle 63 #126-53 050036"
+    },
+    {
+        "nombre": "Frulatto",
+        "facebook": "#",
+        "instagram": "https://www.instagram.com/frulatto_oficial/?hl=es-la",
+        "descripcion": "Deliciosos sabores de helado, con su salsa especial, que te dará una explosión de sabores en cada bocado. ",
+        "horario": "Horario de atención: todos los días de 12:00 a 20:00",
+        "telefono": "304 4626112",
+        "direccion": ""
+    },
+    {
+        "nombre": "En la salida picnic",
+        "facebook": "https://www.facebook.com/enlasalidapicnic",
+        "instagram": "#",
+        "descripcion": "Ambiente campestre para disfrutar en familia una tarde o noche súper agradable con amplia gastronomía y excelente servicio.",
+        "horario": "Horario de atención: Lunes: lunes, martes, miércoles y viernes: cerrado Viernes: 17:00 - 12:00, Sábado y domingo: 0:00 – 12:00",
+        "telefono": "3046545849",
+        "direccion": ""
+    }, {
+        "nombre": "La baguette",
+        "facebook": "#",
+        "instagram": "https://www.instagram.com/labaguette3/?hl=es-la",
+        "descripcion": "Nuestros productos son hechos con las manos de la abuela, materia prima consciente y mucho amor",
+        "telefono": "319 305 27 39",
+        "horario": "",
+        "direccion": ""
+    }, {
+        "nombre": "Pizzachos",
+        "facebook": "https://www.facebook.com/pizzachosyshawarma",
+        "instagram": "#",
+        "descripcion": "Variedad de deliciosas pizzas y comida mexicana sin igual que podrás disfrutar en cada bocado",
+        "horario": "Horario de atención: lunes y martes: cerrado Miércoles, jueves: 17:00 -22:00 Viernes, sábado: 17:00 - 23:00 Domingo: 17:00 – 23:30",
+        "telefono": "311 6608308",
+        "direccion": ""
+    }, {
+        "nombre": "Café express",
+        "facebook": "https://www.facebook.com/cafexpresspapas",
+        "instagram": "https://instagram.com/cafexpresspapas?igshid=txpetnnbtptl",
+        "descripcion": "¡Bienvenido a Café Express! Somos un lugar donde encuentras un amplio Menú de comidas rápidas y helados, en el que puedes disfrutar en un espacio agradable con familia y amigos.🤤😋 Encuéntranos: Al frente de la plazoleta de San Cristóbal.",
+        "horario": "Horario de atención: todos los días: 13:30 a 19:30",
+        "telefono": "304 6788341 – 304 4743692",
+        "direccion": ""
+    },
+    {
+        "nombre": "Jugos y Q´banos del loro",
+        "facebook": "#",
+        "instagram": "#",
+        "descripcion": "Sanduches que podrás disfrutar acompañado de un buen jugo natural que te harán desear repetir su distintivo sabor",
+        "horario": "Horario de atención: todos los días 7:00 – 22:00",
+        "telefono": "312 8429422 por vinser",
+        "direccion": ""
+    },
+    {
+        "nombre": "Punto y coma",
+        "facebook": "https://www.facebook.com/Punto-y-coma-de-todo-un-poco-606016953175780",
+        "instagram": "#",
+        "descripcion": "Tú comida rápida a la puerta de tú casa en tan solo unos minutos de responderte, no te lo pierdas y has tú pedido para que disfrutes más de un poco de todo",
+        "horario": "Horario de atención: todos los días de 12:00 -22:00",
+        "telefono": "3012243134",
+        "direccion": ""
+    },
+    {
+        "nombre": "Carnelly ",
+        "facebook": "#",
+        "instagram": "#",
+        "descripcion": "Disfruta de tu comida rápida al estilo Xiaomi lo mejor en calidad precio ",
+        "horario": "Horario de atención: todos los días 12:00 – 20:00",
+        "telefono": "312 8429422 por vinser",
+        "direccion": "Calle 63 crra 131 #62 16 "
+    },
+    {
+        "nombre": "The ghost ",
+        "facebook": "#",
+        "instagram": "#",
+        "descripcion": "Prueba nuestros granizados con o sin licor y pide al por mayor ",
+        "horario": "Horario de atención: lunes, martes y miércoles: cerrado, Jueves: 19:00 -2:00, Viernes, sábados y domingos: 16:30 – hasta acabar producción",
+        "telefono": "322 5872124",
+        "direccion": " "
+    },
+    {
+        "nombre": "Habibi shawarma",
+        "facebook": "https://www.facebook.com/habibishawarma2019",
+        "instagram": "https://www.instagram.com/habibishawarma2019/?hl=es-la",
+        "descripcion": "Prueba nuestras finas rebanadas de carne y nuestra deliciosa sazón traído del Medio Oriente",
+        "horario": "Horario de atención: lunes y martes: cerrado  Miércoles, jueves: 17:00 -22:00 Viernes, sábado: 17:00 - 23:00 Domingo: 17:00 – 23:30",
+        "telefono": "311 6608308",
+        "direccion": "Calle 62 # 127-96 San Cristóbal, Colombia"
+    }, {
+        "nombre": "Mohos burguer",
+        "facebook": "#",
+        "instagram": "#",
+        "descripcion": "Disfruta nuestras deliciosas hamburguesas artesanales",
+        "horario": "",
+        "telefono": "316 5161298 – 3014119445",
+        "direccion": " "
+    }, {
+        "nombre": "Tiernitas",
+        "facebook": "#",
+        "instagram": "#",
+        "descripcion": "Prueba la gran variedad de arepas y patacones que tenemos para ti ¡no te quedes con las ganas!",
+        "horario": "Horario de atención: todos los días: 12:00 – 22:00",
+        "telefono": "312 8429422 por vinser",
+        "direccion": "Calle 63 crra 131 #62 16"
+    }, {
+        "nombre": "Wings and beer y los que dicen de casa",
+        "facebook": "#",
+        "instagram": "#",
+        "descripcion": "",
+        "horario": "",
+        "telefono": "",
+        "direccion": " "
+    },
+];
+
+
+locales.forEach(e => {
+    $j('#slide-empresas').append(`
+    <li>
+    <div class="team-post">
+        <div class="container">
+            <div class="item-content">
+                <div class="row">
+                    <div class="col-lg-7">
+                        <div class="title">
+                            <span>San Comidas</span>
+                            <h1>${e.nombre}</h1>
+                        </div>
+                        <p>${e.descripcion}</p>
+                        <div class="social-share">
+                            <ul class="social-icons">
+                                <li><a target="_blank" href="${e.facebook}">Facebook</a></li>
+                                <li><a target="_blank" href="${e.instagram}">Instagram</a></li>
+                            </ul>
+                        </div>
+                        <div class="pagination-btn">
+                            <a href="#"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="image-holder">
+            <img src="upload/others/a4.jpg" alt="">
+    </div>
+        </div>
+</li>
+    `);
+
+
+
+});
